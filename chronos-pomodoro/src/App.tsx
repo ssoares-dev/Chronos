9120e225ba1bf36e347e3892e9a46c7ba85b6929
ssoares-dev <ssoares@citeve.pt>
2025-08-06 16:07:40 +0100

    save

---
+++ chronos-pomodoro/src/App.tsx
@@ -0,0 +1,14 @@
+import './styles/global.css'
+import './styles/theme.css'
+import { Heading } from './components/Heading'
+
+function App() {
+
+  return (
+    <>
+      <Heading />
+    </>
+  )
+}
+
+export default App
---
+++ chronos-pomodoro/src/components/Heading.tsx
@@ -0,0 +1,5 @@
+export function Heading() {
+    return <h1 className="heading">Vite + React</h1>;
+}
+
+
